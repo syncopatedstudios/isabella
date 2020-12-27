302b9980d010cdf0d1fe1ef0f2c7b29dcc96581d
b08x <rwpannick@gmail.com>
2020-12-27 00:18:22 -0500

    added link to language model generator

--- lib/listener.rb
+++ lib/listener.rb
@@ -1,21 +1,22 @@
+#!/usr/bin/env ruby
+
 require 'yaml'
 require 'pocketsphinx-ruby'
 
 class Listener
-
   ROOT_FOLDER = File.dirname(__FILE__) + '/../'
   CONFIG_FOLDER = ROOT_FOLDER + 'config/'
 
   FILES = {
-    commands:   CONFIG_FOLDER + 'commands.yml',
+    commands: CONFIG_FOLDER + 'commands.yml',
     dictionary: CONFIG_FOLDER + 'dictionary.dic',
-    grammar:    CONFIG_FOLDER + 'grammar.gram'
-  }
+    grammar: CONFIG_FOLDER + 'grammar.gram'
+  }.freeze
 
   def initialize
     @command_keyphrase = 'hey isabella'
 
-    @commands = YAML::load_file(FILES[:commands])
+    @commands = YAML.load_file(FILES[:commands])
 
     sphinx_config = Pocketsphinx::Configuration::Grammar.new FILES[:grammar]
     sphinx_config['dict'] = FILES[:dictionary]
@@ -32,7 +33,7 @@ class Listener
     end
   end
 
-private
+  private
 
   def respond
     delete_tts_file
@@ -47,19 +48,18 @@ private
   end
 
   def script_and_args
-    [ ROOT_FOLDER + "scripts/#{command['script']}.rb", command_arg.to_s ]
+    [ROOT_FOLDER + "scripts/#{command['script']}.rb", command_arg.to_s]
   end
 
   def command
-    @commands.find { |c| speech_without_command_keyword =~ %r"#{c['pattern']}" }
+    @commands.find { |c| speech_without_command_keyword =~ /#{c['pattern']}/ }
   end
 
   def command_arg
-    speech_without_command_keyword.scan(%r"#{command['pattern']}")[0][0]
+    speech_without_command_keyword.scan(/#{command['pattern']}/)[0][0]
   end
 
   def speech_without_command_keyword
     @speech.sub(/#{@command_keyphrase}\s+/, '')
   end
-
 end
